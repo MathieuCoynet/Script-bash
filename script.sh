d92ab442a53d1de1ae25eac3f3fd8d042f09f69a
MathieuCoynet <m.coynet@laposte.net>
2016-09-28 20:58:20 +0200

    P'tit Script

---
+++ script.sh
@@ -0,0 +1,116 @@
+#!/bin/sh
+
+
+USER=SSH_USERNAME
+HOST=SSH_HOST
+GIT_PATH=SSH_GIT_PATH
+
+REPO=${PWD##*/}
+GIT_REMOTE_URL=ssh://$USER@$HOST/$GIT_PATH/$REPO
+~cd
+read reponse
+echo "==========Bienvenue=Dans=La=Matrice============"
+echo "----------Que cherche-tu l'ami?----------------"
+echo "-----[1]Creer repertoire GITHUB----------------"
+echo "-----[2]Push...================================"
+echo "-----[3]Clone/---------------------------------"
+
+if ("$reponse" = "1"); then 
+
+echo "============================================="
+echo "-------------------------------------------"
+echo "=======Creation=du=repertoire=GITHUB========="
+echo "-------------------------------------------"
+
+# Setup remote repo
+echo "Nous sommes:"
+pwd
+echo "-------------------"
+echo "============Envoyer=repo=à:============"
+echo "-------------------"
+read adresse
+cd ./$adresse/
+
+echo "------------------"
+echo "===========Creation en cour============"
+echo "------------------"
+
+
+# Configure local repo
+
+touch .gitignore
+git init
+git add .
+
+echo "-------Quel est ton commentaire?-------"
+git commit -m '$commentaire'
+read commentaire
+
+echo "---------------"
+echo "============URL?========================="
+echo "---------------"
+
+read url
+git push--all $url 
+git remote add origin $url
+git push u- origin master
+
+echo "----------------------------"
+echo "====OP=bro!========="
+echo "-- -------------------------"
+echo "==========================================="
+
+elif ["$reponse" = "2"]; then
+echo "----------------------------"
+echo "-------------------------------"
+echo "==Que=veux=tu=push?============="
+echo "-------------------------------"
+echo "----------------------------"
+read path
+cd ./$path/
+git add .
+echo "-----------------------------"
+echo "--------------------------------"
+echo "======Objet du changement?========"
+echo "--------------------------------"
+echo "-----------------------------"
+read commantaire
+git commit m- "$commentaire"
+git push
+echo "-----------------------------"
+echo "----------------------------------"
+echo "=======La=matrice=A=bien=reçut=Son=Dù======="
+echo "-----------------------------------"
+echo "-----------------------------"
+
+elif ["$reponse" = "3"]; then
+echo "-----------------------------"
+echo "======Où=veut=Tu=ton=clone=?===="
+echo "-----------------------------"
+read path
+cd ./$path/
+
+echo "----------Endroit-----------------------"
+echo "==De==============Provient=================="
+echo "-----Quelle--------------------Repertoire------"
+echo "===========================Ce==============?==="
+echo "--------------------------------------------"
+echo "---------------------------------------"
+read url
+git clone
+
+echo "-----------------------------"
+echo "-------------------------------------"
+echo "======L'evolution=à=été=cloner=avec=succés==="
+echo "------------------------------------"
+echo "-----------------------------"
+echo "-----------------------------"
+
+else 
+echo "-----------------------------"
+echo "===L=a===="
+echo "--------m=a=t=r=i=c=e==============="
+echo "===========-------------4------"
+echo "--------------====---==-------é-t-é-------"
+echo "-----=------c-o-r=r-o=m-p-u=e--------------------"
+echo "------iIERROR!--!--------------------"
